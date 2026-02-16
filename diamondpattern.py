c6e9f0f4f35dacfd7de65bf35cb48b7b8b5eaf1b
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-16 22:53:08 +0530

    Initial commit

---
+++ diamondpattern.py
@@ -0,0 +1,7 @@
+# To Print Diamond Pattern
+
+n = int(input("Enter n value: "))   # 4
+for i in range(n):                  # 0,1,2,3
+    print(' '*(n-i-1)+'* '*(i+1))
+for i in range(n-1):                  # 0,1,2
+    print(' '*(i+1)+'* '*(n-i-1))
