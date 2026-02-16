385a6a50b18ea4193bab79aa0a4248d1f58f2d79
Jyothi <pulimi.jyothi123@gmail.com>
2026-02-16 23:04:07 +0530

    Initial commit

--- righthalfdiamond.py
+++ righthalfdiamond.py
@@ -1,7 +1,7 @@
 # To print Right Half Diamond pattern with '*' Symbols
 
-n = int(input("Enter a number: "))
-for i in range(n):
+n = int(input("Enter a number: "))  # 4
+for i in range(n):                  # 0,1,2,3
     print('* '*(i+1))
-for i in range(n-1):
+for i in range(n-1):                # 0,1,2
     print('* '*(n-i-1))
